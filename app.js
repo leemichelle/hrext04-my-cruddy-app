5adf69801f8af731ff58a68d99c277fbc359fa08
Chih-Lin Lee <mlee749@gmail.com>
2018-09-12 01:52:16 -0700

    added mark important functionality and adjusted to work with strikethrough and delete

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ var Item = function(name) {
   this.name;
 }
 // create an empty array to push inputs into
+var itemArr = [];
 
 //JSON stringify array to push to localstorage and then parseJSON to retreive array info
+localStorage.setItem(itemKey, JSON.stringify(itemArr))
+var data = JSON.parse(localStorage.getItem(itemKey))
 
-var itemKey = $(".user-input").val();
+var itemKey = $(".user-input").val(); //not used...
 
 
 // function to display items from localStorage
 var listItems = function() {
   var data = localStorage.getItem(itemKey)
   for(var i = 0; i < localStorage.length; i++) {
-    $("ul").append("<li><span><i class ='fa fa-trash'></i></span><span><i class='fa fa-exclamation'></i></span>" + localStorage.key(i) + "</li>")
+    $("ul").append("<li><span><i class ='fa fa-trash'></i></span>" + localStorage.key(i) + "<span><i class='fa fa-exclamation'></i></span></li>")
   }
 }
 
 //invoke function so that page always displays previous localStorage data
 listItems(); 
+
 
 //add item to localStorage
 var addItem = function(name) {
@@ -36,28 +40,33 @@ $(".user-input").keypress(function(e) {
     localStorage.setItem(itemKey, itemKey)
     // localStorage.setItem(itemKey, JSON.stringify(item)); //adds item to localStorage
     $(this).val(""); //resets the input value
-    $("ul").append("<li><span><i class ='fa fa-trash'></i></span><span><i class='fa fa-exclamation'></i></span>" + itemKey + "</li>")
+    $("ul").append("<li><span><i class ='fa fa-trash'></i></span>" + itemKey + "<span><i class='fa fa-exclamation'></i></span></li>")
   }
 })
 
 // delete to-do upon click, also remove from local storage
   // hover over text for option
-$("ul").on("click", "span", function(e) {
-  $(this).parent().fadeOut(function() {
+$("ul").on("click", "i.fa.fa-trash", function(e) { //has to be on ul for all new inputs to work as well
+  $(this).parent().parent().fadeOut(function() { //parent is span, need to be li
     localStorage.removeItem($(this).text()) //retrieves key info from text on 'this' object
     $(this).remove();
   })
 })
 
 // strikethrough to-do upon clicking on text (use toggle to unstrikethrough)
-$("ul").on("click", "li", function() { //specifying all li elements on ul will make event apply to any future new li's
+$("ul").on("dblclick", "li", function() { //specifying all li elements on ul will make event apply to any future new li's
   $(this).toggleClass("completedItem");
 });
 
-// input due date option
 
 // mark important 
   // hover over text for this option
+$("ul").on("click", "i.fa.fa-exclamation", function(e) { //has to be on ul for all new inputs to work as well
+  $(this).parent().parent().toggleClass("important")
+
+})
+
+// input due date option
 
 // add sound icon to play music
 
